61ce5a0899d3b9db7e67e56e2f7c0b783e089d7d
pbk ns <praveenneuron@gmail.com>
2023-12-19 09:48:05 +0100

    ascad v2

--- ours/results_analyze.py
+++ ours/results_analyze.py
@@ -9,7 +9,7 @@ def print_best_hp(_path: str):
     for _npz in pathlib.Path(_path).glob("*"):
         if _npz.name.endswith(".log"):
             continue
-        _data = np.load(_npz)["npz_dict"][()]
+        _data = np.load(_npz, allow_pickle=True)["npz_dict"][()]
         if _data["nt_attack"] < _nt_attack:
             _nt_attack = _data["nt_attack"]
             _best_results = _data
