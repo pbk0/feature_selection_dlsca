2bbaa1fe5201af2d26ab0aecf912513149607f21
pbk ns <praveenneuron@gmail.com>
2023-12-19 09:47:00 +0100

    ascad v2

--- ours/results_analyze.py
+++ ours/results_analyze.py
@@ -4,12 +4,17 @@ import numpy as np
 
 
 def print_best_hp(_path: str):
-    print(_path)
+    _nt_attack = np.inf
+    _best_results = None
     for _npz in pathlib.Path(_path).glob("*"):
         if _npz.name.endswith(".log"):
             continue
-        _data = np.load(_npz)["npz_dict"]
-        print(_data["hp"])
+        _data = np.load(_npz)["npz_dict"][()]
+        if _data["nt_attack"] < _nt_attack:
+            _nt_attack = _data["nt_attack"]
+            _best_results = _data
+    for _k, _v in _best_results.items():
+        print(_k, _v)
     
 
 
