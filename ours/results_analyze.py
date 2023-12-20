96f92eab8262942f3cd6b61ae959bd656da1cedc
pbk ns <praveenneuron@gmail.com>
2023-12-20 07:59:18 +0100

    update random_search dir

--- ours/results_analyze.py
+++ ours/results_analyze.py
@@ -1,6 +1,11 @@
+import subprocess
 import sys
 import pathlib
 import numpy as np
+
+import matplotlib
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 
 def print_best_hp(_path: str):
@@ -43,7 +48,41 @@ def best_model_runs(_path: str):
         _results[_model_type][_lk_model].append(
             _data["nt_attack"]
         )
-    print(_results)
+    
+    # customizing runtime configuration stored
+    # in matplotlib.rcParams
+    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+    
+    fig1 = plt.figure()
+    plt.plot([17, 45, 7, 8, 7], color='orange')
+    
+    fig2 = plt.figure()
+    plt.plot([13, 25, 1, 6, 3], color='blue')
+    
+    Fig3 = plt.figure()
+    plt.plot([22, 11, 2, 1, 23], color='green')
+    
+    # PdfPages is a wrapper around pdf
+    # file so there is no clash and create
+    # files with no error.
+    _pdf_file = pathlib.Path(_path) / "best_model_runs.pdf"
+    p = PdfPages(_pdf_file)
+    
+    # get_fignums Return list of existing
+    # figure numbers
+    fig_nums = plt.get_fignums()
+    figs = [plt.figure(n) for n in fig_nums]
+    
+    # iterating over the numbers in list
+    for fig in figs:
+        # and saving the files
+        fig.savefig(p, format='pdf')
+        
+        # close the object
+    p.close()
+    
+    subprocess.run(["xdg-open", _pdf_file.absolute().resolve().as_posix()])
     
 
 if __name__ == "__main__":
